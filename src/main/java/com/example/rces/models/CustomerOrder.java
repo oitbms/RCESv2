187af0c7c872b0052f4bba214960e099c1593807
Bormash <it@bormash.ru>
2025-03-10 16:51:31 +0300

    #7 Исправил, улучшил

--- src/main/java/com/example/rces/models/CustomerOrder.java
+++ src/main/java/com/example/rces/models/CustomerOrder.java
@@ -4,7 +4,6 @@ package com.example.rces.models;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,16 +11,13 @@ import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
-@Table(name = "customerorder",
-        uniqueConstraints={
-                @UniqueConstraint(name="bk_dm_customerorder_str_code_idx", columnNames = {"str_code"})
-        })
+@Table(name = "customerorder")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerOrder extends EntityBase {
 
-    @Column(name="str_code")
+    @Column(name = "str_code")
     private String code; // Номер
 
 }
