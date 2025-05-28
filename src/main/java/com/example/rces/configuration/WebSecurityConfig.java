b49fb8f6c6988d146298a147d90af7a78477277b
Bormash <it@bormash.ru>
2025-05-28 09:55:35 +0300

    #88 Проблемы с доступом к

--- src/main/java/com/example/rces/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/rces/configuration/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig {
                         .requestMatchers("/home").hasAnyAuthority("TECHNOLOGIST", "OTK", "CONSTRUCTOR", "ADMIN", "MASTER")
                         .requestMatchers("/admin", "/registration").hasAuthority("ADMIN")
                         .requestMatchers("/create", "/requestslist/**")
-                        .hasAnyAuthority("MASTER", "ADMIN","CONSTRUCTOR", "TECHNOLOGIST", "OTK")
+                        .hasAnyAuthority("MASTER", "ADMIN","CONSTRUCTOR", "TECHNOLOGIST", "OTK", "CONTROL")
                         .requestMatchers("/sgi/**").hasAnyAuthority("ADMIN", "CONTROL", "EVENT")
                         .requestMatchers(new TypeBasedRequestMatcher(service)).authenticated()
                         .anyRequest().authenticated()
