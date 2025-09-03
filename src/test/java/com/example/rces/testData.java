2d4dda1dc206cf395f99773ceae7dc328d3faf4e
ByteCodeAPAAPA <ilya26.khohlov@gmail.com>
2025-09-03 11:53:54 +0300

    #139 4% покрытие тестами

--- src/test/java/com/example/rces/testData.java
+++ src/test/java/com/example/rces/testData.java
@@ -1,30 +1,53 @@
-//package com.example.rces;
-//
-//import com.example.rces.models.Employee;
-//import com.example.rces.models.enums.Role;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class testData {
-//
-//    static Employee getCreateEmployee() {
-//        Employee employee = new Employee();
-//        employee.setId(1L);
-//        employee.setName("TEST");
-//        employee.setChatId(1L);
-//        employee.setPassword(String.valueOf(123456));
-//        employee.setActive(true);
-//        employee.setRole(Role.USER.getName());
-//        return employee;
-//    }
-//
-//    public static List<Employee> getListEmployees() {
-//        List<Employee> employees = new ArrayList<>();
-//        employees.add(getCreateEmployee());
-//        employees.add(getCreateEmployee());
-//        employees.add(getCreateEmployee());
-//        return employees;
-//    }
-//
-//}
+package com.example.rces;
+
+import com.example.rces.models.CustomerOrder;
+import com.example.rces.models.Employee;
+import com.example.rces.models.SGI;
+import com.example.rces.models.enums.MlmNode;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public class testData {
+
+    public static List<SGI> createTestSgiList() {
+        SGI sgi = new SGI();
+        sgi.setId(UUID.randomUUID());
+        sgi.setRequestNumber(1);
+        sgi.setWorkShop("Цех 1");
+        sgi.setEvent("Тестовое мероприятие");
+        sgi.setActions("Тестовые действия");
+        sgi.setDepartment(SGI.Department.mechanic);
+        sgi.setDesiredDate(LocalDate.now());
+        sgi.setPlanDate(LocalDate.now().plusDays(1));
+        sgi.setNote("Тестовая заметка");
+        sgi.setComment("Тестовый комментарий");
+        sgi.setAgreed(true);
+        sgi.setColor(SGI.ColorSGI.GREEN);
+
+        return List.of(sgi);
+    }
+
+    public static Employee createEmployee() {
+        Employee employee = new Employee();
+        employee.setId(1L);
+        employee.setName("Test");
+        employee.setRole("ADMIN");
+        employee.setPassword("123456");
+        employee.setActive(true);
+        employee.setMlmNode(MlmNode.workShop1);
+        employee.setChatId(-1L);
+        return employee;
+    }
+
+    public static CustomerOrder createCustomerOrder() {
+        CustomerOrder customerOrder = new CustomerOrder();
+        customerOrder.setId(UUID.randomUUID());
+        customerOrder.setName("TestCustomerOrderName");
+        customerOrder.setCreateDate(LocalDateTime.now());
+        customerOrder.setEmployee(createEmployee());
+        return customerOrder;
+    }
+}
